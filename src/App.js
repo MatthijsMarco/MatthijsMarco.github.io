c72e347543c14bd847c3bebcb9fbc8f2fbc88742
Marco Matthijs <marco.matthijs@student.hogent.be>
2022-10-19 16:52:53 +0200

    Random Change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Push to auto-deploy to Github pages.
+          Edit <code>src/App.js</code> and save to reload. Push to auto-deploy to Github pages. Random change.
         </p>
         <a
           className="App-link"
